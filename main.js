25614d9e128680fb33885f1c75ad75eb40392ba8
Kevincango <kevincantugomez98@gmail.com>
2023-10-10 17:06:43 -0600

    get donde, implementation of post in-proccess

--- main.js
+++ main.js
@@ -1,16 +1,75 @@
-const API = 'https://api.thedogapi.com/v1/images/search?limit=3&api_key=live_EcXKvj1wjgx3bJKWy3DYAurrVwz1YpyDYoFwwtJzssg59HJC6eMwmBrQj9udNPow';
+const API = 'https://api.thedogapi.com/v1/images/search?limit=2';
+const FAVOURITES = 'https://api.thedogapi.com/v1/favourites';
+const API_KEY = 'live_4HbFHf3sbc7Vt4Ort9m5sntl0MFo8QnfOazYLAC3xP8znArBVBTNpPY7RlxiQ5NW';
 
-async function fetchData(urlApi) {
-    const response = await fetch(urlApi);
-    const data = await response.json();
-    const dogImg1 = document.getElementById('img1');
-    const dogImg2 = document.getElementById('img2');
-    const dogImg3 = document.getElementById('img3');
-    dogImg1.src = data[0].url;
-    dogImg2.src = data[1].url;
-    dogImg3.src = data[2].url;
+const randomError = document.getElementById('error');
+
+async function loadRandomDogs(urlApi) {
+    try {
+        const response = await fetch(urlApi, {
+            headers: {
+                'x-api-key': API_KEY
+            }
+        });
+        const data = await response.json();
+        
+        if(response.status !== 200) {
+            randomError.innerText = `Hubo un error ${response.status}`;
+            return;
+        }
+
+        const dogImg1 = document.getElementById('random-img1');
+        const dogImg2 = document.getElementById('random-img2');
+
+        if(data.length > 0) dogImg1.src = data[0].url;
+        if(data.length > 1) dogImg2.src = data[1].url;
+
+    } catch (error) {
+        randomError.innerText = `Hubo un error: ${error.message}`;
+    }
 }
 
-fetchData(API);
-const dogButton = document.querySelector('.dog-button');
-dogButton.addEventListener('click', ()=> fetchData(API));
+async function loadFavoritesDogs() {
+    try {
+        const response = await fetch(FAVOURITES, {
+            headers: {
+                'x-api-key': API_KEY
+            }
+        });
+        const data = await response.json();
+
+        if(response.status !== 200) {
+            randomError.innerText = `There are an error ${response.status}: ${data.message}`;
+            return;
+        }
+
+        console.log(data);
+    } catch (error) {
+        randomError.innerText = `Hubo un error: ${error.message}`;
+    }
+}
+
+async function saveFavouriteDogs(){
+    const response = await fetch(FAVOURITES, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'x-api-key': API_KEY
+        },
+        body: JSON.stringify({
+          image_id: 'dje' // Id of dog to save
+        }),
+    })
+    const data = await response.json();
+    if(response.status !== 200){
+        randomError.innerText = `Hubo un error ${response.status}: ${data.message}`;
+    }
+}
+
+loadRandomDogs(API);
+
+const dogRandomButton = document.querySelector('.dog-button');
+dogRandomButton.addEventListener('click', () => loadRandomDogs(API));
+
+
+
